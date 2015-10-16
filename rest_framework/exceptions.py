ab240f5d32142c9bdfc402e4217c7a2e023c737d
Amin Mirzaee <amin@chide.it>
2015-10-16 03:32:08 -0400

    Add in ValidationError from https://github.com/tomchristie/django-rest-framework/blob/05cbec9dd7f9f0b6a9b59b29ac6c9272b6ae50d8/rest_framework/exceptions.py

--- rest_framework/exceptions.py
+++ rest_framework/exceptions.py
@@ -23,6 +23,25 @@ class APIException(Exception):
     def __str__(self):
         return self.detail
 
+# The recommended style for using `ValidationError` is to keep it namespaced
+# under `serializers`, in order to minimize potential confusion with Django's
+# built in `ValidationError`. For example:
+#
+# from rest_framework import serializers
+# raise serializers.ValidationError('Value was invalid')
+
+class ValidationError(APIException):
+    status_code = status.HTTP_400_BAD_REQUEST
+
+    def __init__(self, detail):
+        # For validation errors the 'detail' key is always required.
+        # The details should always be coerced to a list if not already.
+        if not isinstance(detail, dict) and not isinstance(detail, list):
+            detail = [detail]
+        self.detail = detail
+
+    def __str__(self):
+        return str(self.detail)
 
 class ParseError(APIException):
     status_code = status.HTTP_400_BAD_REQUEST
